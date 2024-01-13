85443c12b56ddf1d1a916809dcd35dc4a31af868
zm20355 <131091759+zm20355@users.noreply.github.com>
2024-01-13 12:06:18 +0900

    Update ufc.js

--- ufc.js
+++ ufc.js
@@ -1,2 +1,2 @@
 function a1(){document.getElementById('omaly').src='./image/omalley.jpeg';}
-function a2(){document.getElementById('omaly').src='./image/ufc.jpg';}
+function a2(){document.getElementById('omaly').src='./image/UFC.jpg';}
